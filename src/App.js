297fdd62caae79f8b06a078e1790442bd5894a77
JohnJelly <taylorjelly@gmail.com>
2023-04-05 17:52:01 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+/* eslint-disable react/jsx-key */
+import React from 'react';
+import {useEffect, useState} from 'react';
+import MovieCard from './MovieCard';
+import './App.css';
+import SearchIcon from './search.svg';
+
+// omdb api key: 3536c302
+const apiUrl = 'http://www.omdbapi.com?apikey=3536c302';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}` );
+    const data = await response.json();
+    setMovies(data.Search);
+    console.log(data);
+  };
+
+  const handleEnterPress = (event) => {
+    if (event.key === 'Enter') {
+      searchMovies(searchTerm);
+    }
+  };
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          onKeyDown={handleEnterPress}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => {
+            searchMovies(searchTerm);
+          }}
+        />
+      </div>
+      {
+        movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
+    </div>
+  );
+};
+
+export default App;
